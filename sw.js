07780ae3a77faff0d0d3179b5df63cc584ca31fa
complete <bean12810@gmail.com>
2026-04-26 21:19:23 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -8,38 +8,28 @@ const ASSETS_TO_CACHE = [
   "/planner/manifest.json"
 ];
 
-// Install
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS_TO_CACHE);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
   );
 });
 
-// Activate
 self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
+    caches.keys().then((keys) =>
+      Promise.all(
         keys
           .filter((key) => key !== CACHE_NAME)
           .map((key) => caches.delete(key))
-      );
-    })
+      )
+    )
   );
 });
 
-// Fetch
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((cached) => {
-      return (
-        cached ||
-        fetch(event.request).catch(() => {
-          // Optional: offline fallback here
-        })
-      );
+      return cached || fetch(event.request).catch(() => {});
     })
   );
 });
